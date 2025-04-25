1ee89a87bd86f484cbc40d7f200b224b953ef989
carlos velez <gpmotos1@gmail.com>
2025-04-24 22:17:26 -0500

    🚀 files that were causing problems are corrected

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="proyectoInt_V_2025",
+    version="0.0.1",
+    author="Carlos Alberto Velez",
+    author_email="",
+    description="",
+    py_modules=["actividad_1","actividad_2"],
+    install_requires=[
+        "pandas",
+        "numpy",
+        "openpyxl",
+        "requests"
+    ]
+)
